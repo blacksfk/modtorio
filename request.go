179d21a9ea8484aebd24d2b119c5a6f84da6a1c9
blacksfk <blacksunflowakuttaspam@hotmail.com>
2020-02-11 16:30:35 +1100

    login() now returns a (string, error) instead of an object

--- request.go
+++ request.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"strings"
 	"net/url"
 	"net/http"
 	"io/ioutil"
@@ -8,21 +10,30 @@ import (
 )
 
 const (
-	loginUrl = "https://auth.factorio.com/api-login"
+	URL_LOGIN = "https://auth.factorio.com/api-login"
 )
 
-// factorio.com authentication data
-type Auth struct {
-	Token string
+type ApiError struct {
+	Message string
 }
 
 // log the user in with an HTTP POST request
-func login(username, password string) (*Auth, error) {
-	data := url.Values{username: {username}, password: {password}}
-	res, err := http.PostForm(loginUrl, data)
+func login(username, password string) (string, error) {
+	data := url.Values{}
+
+	data.Set("username", username)
+	data.Set("password", password)
+
+	// create the request and client
+	req, err := http.NewRequest("POST", URL_LOGIN, strings.NewReader(data.Encode()))
+	client := http.Client{}
+
+	// add the content type header and send the request
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	res, err := client.Do(req)
 
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
 	// read the response data
@@ -30,17 +41,32 @@ func login(username, password string) (*Auth, error) {
 	res.Body.Close()
 
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
-	// convert the response to JSON
-	auth := &Auth{}
-	err = json.Unmarshal(body, auth)
+	if res.StatusCode >= 400 {
+		// something went wrong with the request
+		ae := &ApiError{}
+		err = json.Unmarshal(body, ae)
+
+		if err != nil {
+			// conversion to JSON failed
+			return "", err
+		}
+
+		// return the API error
+		return "", fmt.Errorf("%s: %s", res.Status, ae.Message)
+	}
+
+	// request/response was all good, convert to JSON
+	var resData []string
+	err = json.Unmarshal(body, &resData)
 
 	if err != nil {
-		return nil, err
+		// conversion to JSON failed
+		return "", err
 	}
 
 	// all is good, return no error
-	return auth, nil
+	return resData[0], nil
 }
