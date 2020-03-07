dbe929eb4ac46f250d4dabbeafc7a787ed7e5d4a
blacksfk <blacksunflowakuttaspam@hotmail.com>
2020-03-07 20:38:59 +1100

    updated help text function with commands, descriptions, and examples

--- main.go
+++ main.go
@@ -46,7 +46,65 @@ func main() {
 
 // display help for program usage
 func help() {
-	fmt.Println("Usage:")
-	fmt.Printf("\tmodtorio <command> [...options] <arguments>\n")
-	fmt.Printf("\tCommands: search, download, help\n")
+	fmt.Printf("usage: modtorio [...flags] <command> [...options] <arguments>\n\n")
+
+	fmt.Printf("Flags:\n")
+	fmt.Printf("\t--dir\tSpecify the working directory for commands that interact with modlist.json. Leave blank if the current directory contains modlist.json\n")
+	fmt.Printf("\t--factorio\tSpecify the factorio version to compare releases against. Defaults to the latest version\n\n")
+
+	fmt.Printf("Commands:\n")
+
+	// search command
+	fmt.Printf("search\n")
+	fmt.Printf("\tSearch for a mod. The argument is compiled as a regular expression.\n")
+	fmt.Printf("\tOptions:\n")
+	fmt.Printf("\t\t--tag\tSearch for mods based on a tag\n")
+	fmt.Printf("\t\t--owner\tSearch for mods created by a user\n")
+	fmt.Printf("\tExamples:\n")
+	fmt.Printf("\t\tmodtorio search ^bob\n")
+	fmt.Printf("\t\tmodtorio search --tag general\n")
+	fmt.Printf("\t\tmodtorio search --owner py*\n")
+
+	// download command
+	fmt.Printf("download\n")
+	fmt.Printf("\tDownload any number of mods. Must be listed by the mod name.\n")
+	fmt.Printf("\tExamples:\n")
+	fmt.Printf("\t\tmodtorio download bobinserters miniloader pyhightech\n")
+	fmt.Printf("\t\tmodtorio --factorio 0.17 --dir ~/.config/factorio/mods download bobinserters helicopters\n")
+
+	// update command
+	fmt.Printf("update\n")
+	fmt.Printf("\tUpdate all mods to their latest release for the factorio version (if specified).\n")
+	fmt.Printf("\tExamples:\n")
+	fmt.Printf("\t\tmodtorio update\n")
+	fmt.Printf("\t\tmodtorio --factorio 0.18 update\n")
+	fmt.Printf("\t\tmodtorio --factorio 0.18 --dir ~/.config/factorio/mods update\n")
+
+	// enable command
+	fmt.Printf("enable\n")
+	fmt.Printf("\tEnable mods. Arguments are compiled as regular expressions.\n")
+	fmt.Printf("\tExamples:\n")
+	fmt.Printf("\t\tmodtorio enable bob* pyhightech ^angel\n")
+	fmt.Printf("\t\tmodtorio --dir ~/.config/factorio/mods enable bob*\n")
+
+	// disable command
+	fmt.Printf("disable\n")
+	fmt.Printf("\tDisable mods. Arguments are compiled as regular expressions.\n")
+	fmt.Printf("\tExamples:\n")
+	fmt.Printf("\t\tmodtorio disable bob* pyhightech ^angel\n")
+	fmt.Printf("\t\tmodtorio --dir ~/.config/factorio/mods disable bob*\n")
+
+	// list-enabled command
+	fmt.Printf("list-enabled\n")
+	fmt.Printf("\tList all enabled mods.\n")
+	fmt.Printf("\tExamples:\n")
+	fmt.Printf("\t\tmodtorio list-enabled\n")
+	fmt.Printf("\t\tmodtorio --dir ~/.config/factorio/mods list-enabled\n")
+
+	// list-disabled command
+	fmt.Printf("list-disabled\n")
+	fmt.Printf("\tList all disabled mods.\n")
+	fmt.Printf("\tExamples:\n")
+	fmt.Printf("\t\tmodtorio list-disabled\n")
+	fmt.Printf("\t\tmodtorio --dir ~/.config/factorio/mods list-disabled\n")
 }
