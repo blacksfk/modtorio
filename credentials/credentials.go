ad48ae4d003f9a1512c55219b2060387f2c0f9bf
blacksfk <blacksunflowakuttaspam@hotmail.com>
2020-03-24 15:38:21 +1100

    added credential caching in the current working directory

--- credentials/credentials.go
+++ credentials/credentials.go
@@ -4,10 +4,22 @@ auth.factorio.com web API.
 */
 package credentials
 
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+const (
+	MODE  = 0600
+	CACHE = "./modtorio_user.json"
+)
+
 type Credentials struct {
-	Username, Password, Token string
+	Username, Token string
+	Password        string `json:"-"` // don't store the user's password
 }
 
+// Create a new set of credentials (minus the token).
 func NewCredentials(username, password string) *Credentials {
 	creds := Credentials{}
 
@@ -15,4 +27,33 @@ func NewCredentials(username, password string) *Credentials {
 	creds.Password = password
 
 	return &creds
+}
+
+// Get a set of credentials from the cache.
+func FromCache() (*Credentials, error) {
+	bytes, e := ioutil.ReadFile(CACHE)
+
+	if e != nil {
+		return nil, e
+	}
+
+	creds := &Credentials{}
+	e = json.Unmarshal(bytes, creds)
+
+	if e != nil {
+		return nil, e
+	}
+
+	return creds, nil
+}
+
+// Write a set of credentials to the cache (minus the password).
+func (c *Credentials) ToCache() error {
+	bytes, e := json.Marshal(c)
+
+	if e != nil {
+		return e
+	}
+
+	return ioutil.WriteFile(CACHE, bytes, MODE)
 }
