09f9438ea6a3e39dffe2e8a101fdd482cedf86d2
blacksfk <blacksunflowakuttaspam@hotmail.com>
2020-03-26 19:21:44 +1100

    reduced GetAll() to one HTTP request by using page_size=max parameter

--- api/mod.go
+++ api/mod.go
@@ -3,55 +3,42 @@ package api
 import (
 	"encoding/json"
 	"net/http"
-	"strconv"
 	"strings"
 )
 
 const (
-	URL_MOD = "https://mods.factorio.com/api/mods"
+	PAGE_SIZE = "?page_size=max"
+	URL_MOD   = "https://mods.factorio.com/api/mods"
 )
 
-// get all mods
+// get all mods or mods exactly matching the names provided
 func GetAll(names ...string) ([]*Result, error) {
-	var e error
-	var size int
-	namelist := strings.Builder{}
 	count := len(names)
+	url := strings.Builder{}
+	namelist := strings.Builder{}
 
-	if count == 0 {
-		// no mod names passed so get all mods
-		size, e = getModCount()
-	} else {
-		size = count
+	// build a string of mod names separated by a comma
+	for i := 0; i < count; i++ {
+		namelist.WriteString(names[i])
 
-		// build a string of mod names separated by a comma
-		for i := 0; i < count; i++ {
-			namelist.WriteString(names[i])
-
-			if i < count-1 {
-				// only append a comma if not the last element
-				namelist.WriteString(",")
-			}
+		if i < count-1 {
+			// only append a comma if not the last element
+			namelist.WriteString(",")
 		}
 	}
 
-	if e != nil {
-		return nil, e
-	}
-
-	url := strings.Builder{}
+	// build the initial URL string
 	url.WriteString(URL_MOD)
-	url.WriteString("?page_size=")
-	url.WriteString(strconv.Itoa(size))
+	url.WriteString(PAGE_SIZE)
 
-	// only append &namelist=<list> if count > 0
+	// only append &namelist=<list> if mod names were provided
 	if count > 0 {
 		url.WriteString("&namelist=")
 		url.WriteString(namelist.String())
 	}
 
 	// get all mods in one shot by requesting a "page" with all of the mods
-	// i.e. page_size=<no_of_mods>
+	// i.e. page_size=max
 	res, e := http.Get(url.String())
 
 	if e != nil {
@@ -64,17 +51,17 @@ func GetAll(names ...string) ([]*Result, error) {
 		return nil, e
 	}
 
-	modList := &ModListResponse{}
-	e = json.Unmarshal(body, modList)
+	mlr := &ModListResponse{}
+	e = json.Unmarshal(body, mlr)
 
 	if e != nil {
 		return nil, e
 	}
 
-	// parse the factorio version of each release as a semver
-	for _, result := range modList.Results {
+	// parse the factorio and release versions as semvers
+	for _, result := range mlr.Results {
 		for _, release := range result.Releases {
-			e = release.ParseVersions()
+			e := release.ParseVersions()
 
 			if e != nil {
 				return nil, e
@@ -82,31 +69,5 @@ func GetAll(names ...string) ([]*Result, error) {
 		}
 	}
 
-	// no need to return pagination data
-	return modList.Results, nil
-}
-
-// get the count of mods
-func getModCount() (int, error) {
-	// get the count by sending a request with only one mod per page
-	res, e := http.Get(URL_MOD + "?page_size=1")
-
-	if e != nil {
-		return 0, e
-	}
-
-	body, e := handleResponse(res)
-
-	if e != nil {
-		return 0, e
-	}
-
-	modList := &ModListResponse{}
-	e = json.Unmarshal(body, modList)
-
-	if e != nil {
-		return 0, e
-	}
-
-	return modList.Pagination.Page_count, nil
+	return mlr.Results, nil
 }
